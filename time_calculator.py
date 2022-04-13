5feb5b8f46313d3514cea5e3c3d3543967fbf925
mytranbui <mytranbui@hotmail.com>
2022-04-13 00:03:41 -0400

    ok

--- time_calculator.py
+++ time_calculator.py
@@ -1,6 +1,56 @@
-def which_day(day, add_day):
+def monday():
+    return "Monday"
+def tuesday():
+    return "Tuesday"
+def wednesday():
+    return "Wednesday"
+def thursday():
+    return "Thursday"
+def friday():
+    return "Friday"
+def saturday():
+    return "Saturday"
+def sunday():
+    return "Sunday"
+def default():
+    return "Incorrect day"
 
-    return this_day
+weekday = {
+    0: default,
+    1: monday,
+    2: tuesday,
+    3: wednesday,
+    4: thursday,
+    5: friday,
+    6: saturday,
+    7: sunday
+    }
+
+def match_day(day):
+    match day:
+        case "Monday":
+            return 1
+        case "Tuesday":
+            return 2
+        case "Wednesday":
+            return 3
+        case "Thursday":
+            return 4
+        case "Friday":
+            return 5
+        case "Saturday":
+            return 6
+        case "Sunday":
+            return 7
+        case _:
+            return 0
+
+def which_day(day, add_day):
+    nbr = (match_day(day) + add_day % 7) % 7
+    nbr = 7 if nbr == 0 else nbr
+    print(nbr)
+    day = weekday[nbr]()
+    return day
 
 def add_time(start, duration, day=None):
     print(start, duration, day)
@@ -29,11 +79,10 @@ def add_time(start, duration, day=None):
     # print(total_hr," hrs")
     total_hr %= 12
     # print(total_hr," hrs")
-    if total_hr == 0:
-        total_hr = 12
+    total_hr = 12 if total_hr == 0 else total_hr
     new_time = str(total_hr) + ':' + str(total_min % 60).rjust(2, '0') + ' ' + ampm
     if day != None:
-        day = day[0].upper() + day[1:].lower()
+        day = which_day(day[0].upper() + day[1:].lower(), next_day)
         new_time += ", " + day
     if next_day == 1:
         new_time += " (next day)"
