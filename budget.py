a6878af343dea0eb0e36c5a2ba78ec1af885ae00
mytranbui <mytranbui@hotmail.com>
2022-05-18 16:30:55 +0200

    print out vertical cat now o

--- budget.py
+++ budget.py
@@ -1,3 +1,4 @@
+from nis import cat
 import budget
 import re
 class Category:
@@ -51,11 +52,13 @@ def create_spend_chart(categories):
   spending_all = []
   spending_by_cat = []
   perc_lst = []
+  # print(str(categories))
   for category in categories:
     spending_all.append(re.findall(r'-\d*\.?\d+', str(category)))
-    name_cat.append(re.findall('[A-Z][a-z]+', str(category)[:30]))
-  # print(spending_all)
-  # print(name_cat)
+    name_cat.append(re.findall('\*([A-Z][a-z]+)', str(category)[:30]))
+  # name_cat.append(re.findall('[A-Z][a-z]+', str(categories[0-3])))
+  print(spending_all)
+  print(name_cat)
   for lst in spending_all:
     spending = 0
     for str_nb in lst:
@@ -66,17 +69,74 @@ def create_spend_chart(categories):
   # print(total_spending)
   for spending in spending_by_cat:
     perc_lst.append(round(spending / total_spending * 10))
-  # print(perc_lst)
+  print(perc_lst)
   bar_chart = ""
   bar_chart += "Percentage spent by category\n"
   perc = 100
   while perc != 0:
     bar_chart += str(perc).rjust(3) + "|".ljust(len(name_cat) * 4 - 1) + 'END' + '\n'
     perc -= 10
-  bar_chart += ' ' * 4 + '-' * (len(name_cat) * 3 + 1)
-  for name in name_cat:
-    for char in name:
-      bar_chart += 
+  bar_chart += ' ' * 4 + '-' * (len(name_cat) * 3 + 1) + '\n'
+  lst = []
+  i = 0
+  while i < len(name_cat):
+    lst.append(name_cat[i][0])
+    i += 1
+  longest_cat = max(len(lst[0]),len(lst[1]))
+  print(longest_cat)
+  vertical_cat = ""
+  for i in range(longest_cat):
+    vertical_cat += "    "
+    for name in lst :
+      try :
+        vertical_cat += name[i].center(3)
+      except IndexError:
+        vertical_cat += "   "
+    vertical_cat += "\n"
+  vertical_cat += "           "
+   # added to match the amount of characters needed
+  bar_chart += vertical_cat
+  # j = 0
+  # for lst in name_cat:
+  #   for name in lst:
+  #     print("yo",name[0])
+  #     print("po",name[0][0])
+    # while j < len(name):
+    # for name in lst:
+    #   i = 0
+    #   bar_chart += 'xxx'
+    #   while i < len(name_cat):
+    #     bar_chart += name[i].rjust(3)
+    #     i += 1
+    #   # for char  in name:
+    #   #   bar_chart += char.rjust(2)
+    #   bar_chart += 'xx\n'
+  # for lst in name_cat:
+  #   for name in lst:
+  #     i = 0
+  #     bar_chart += 'xxx'
+  #     while i < len(name_cat):
+  #       bar_chart += name[i].rjust(3)
+  #       i += 1
+  #     # for char  in name:
+  #     #   bar_chart += char.rjust(2)
+  #     bar_chart += 'xx\n'
+  # i = 0
+  # j = 0
+  # while i <= len(name_cat):
+  #   # if name_cat[i][0][j]:
+  #   print('j',j)
+  #   # if j >= len(name_cat[i][0]):
+  #   #   print("continue")
+  #   #   j=0
+  #   #   print('xx')
+  #     # continue
+  #   print(name_cat[i][0][j])
+  #   if i == len(name_cat) - 1:
+  #     i = 0
+  #     j += 1
+  #   i+=1
+  # print(len(name_cat))
   # print("100|          " + "ENDhkjh")
   return bar_chart
 
