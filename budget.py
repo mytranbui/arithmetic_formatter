09144146d7823390a91645ca3930e3c1db92cc7b
mytranbui <mytranbui@hotmail.com>
2022-05-22 18:27:03 +0200

    okok

--- budget.py
+++ budget.py
@@ -43,9 +43,9 @@ class Category:
     string += "Total: " + str(format(float(self.balance), '.2f'))
     return string
 
-  def print_ledger(self):
-    print(self.name)
-    print(self.ledger)
+  # def print_ledger(self):
+  #   print(self.name)
+  #   print(self.ledger)
 
 def longest_len_str(lst):
   longest = ""
@@ -76,7 +76,7 @@ def create_spend_chart(categories):
   # print(total_spending)
   for spending in spending_by_cat:
     perc_lst.append((spending / total_spending) * 100)
-  print(perc_lst)
+  # print(perc_lst)
   bar_chart = ""
   bar_chart += "Percentage spent by category\n"
   lst = []
@@ -96,7 +96,6 @@ def create_spend_chart(categories):
     perc -= 10
   bar_chart += ' ' * 4 + '-' * (len(lst) * 3 + 1) + '\n'
   longest_cat = longest_len_str(lst)
-  # longest_cat = max(len(lst[0]),len(lst[1]))
   # print(longest_cat)
   vertical_cat = ""
   for i in range(longest_cat):
@@ -106,56 +105,41 @@ def create_spend_chart(categories):
         vertical_cat += name[i].center(3)
       except IndexError:
         vertical_cat += "   "
-    vertical_cat += " \n"
+    vertical_cat += " "
+    if i != longest_cat -1 :
+      vertical_cat += "\n"
   bar_chart += vertical_cat
+  # print(longest_cat)
   return bar_chart
 
-#         # m  \n           e  \n           n  \n           t  \n' 
-#         # m  \n           e  \n           n  \n           t  '
+# food = budget.Category("Food")
+# food.deposit(1000, "initial deposit")
+# food.withdraw(10.15, "groceries")
+# food.withdraw(15.89, "restaurant and more food for dessert")
+# # print(food.get_balance())
+# clothing = budget.Category("Clothing")
+# food.transfer(50, clothing)
+# clothing.withdraw(25.55)
+# clothing.withdraw(100)
+# auto = budget.Category("Auto")
+# auto.deposit(1000, "initial deposit")
+# auto.withdraw(15)
 
-  # for lst in name_cat:
-  #   for name in lst:
-  #     i = 0
-  #     bar_chart += 'xxx'
-  #     while i < len(name_cat):
-  #       bar_chart += name[i].rjust(3)
-  #       i += 1
-  #     # for char  in name:
-  #     #   bar_chart += char.rjust(2)
-  #     bar_chart += 'xx\n'
-  # i = 0
-  # j = 0
-  # while i <= len(name_cat):
-  #   # if name_cat[i][0][j]:
-  #   print('j',j)
-  #   # if j >= len(name_cat[i][0]):
-  #   #   print("continue")
-  #   #   j=0
-  #   #   print('xx')
-  #     # continue
-  #   print(name_cat[i][0][j])
-  #   if i == len(name_cat) - 1:
-  #     i = 0
-  #     j += 1
-  #   i+=1
-  # print(len(name_cat))
-  # print("100|          " + "ENDhkjh")
-  # return bar_chart
+# print(food)
+# print(clothing)
+# print(auto)
+# print(create_spend_chart([food, clothing, auto]))
 
-food = budget.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-# print(food.get_balance())
-clothing = budget.Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = budget.Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
-
-print(food)
-print(clothing)
-print(auto)
-print(create_spend_chart([food, clothing, auto]))
+# business = budget.Category("Business")
+# food = budget.Category("Food")
+# entertainment = budget.Category("Entertainment")
+# food.deposit(900, "deposit")
+# entertainment.deposit(900, "deposit")
+# business.deposit(900, "deposit")
+# food.withdraw(105.55)
+# entertainment.withdraw(33.40)
+# business.withdraw(10.99)
+# print(business)
+# print(food)
+# print(entertainment)
+# print(create_spend_chart([business, food, entertainment]))
